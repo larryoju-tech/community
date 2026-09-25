95ebc39aea5258e536c1bb76605c4e22e5aa33ab
larryoju-tech <larryoju@gmail.com>
2026-09-25 16:28:30 +0000

    Created By Me

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 
-import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO # import Raspberry Pi GPIO library
 import time
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)# Ignore warning for now
+GPIO.setmode(GPIO.BOARD) #Use physical pin numbering
+# Set General Purpose Input Output(GPIO) Pin 4 (position 7) to be an input pin and set initial value to be pulled low(off)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 button_pressed = False
 
-while True:
+while True: # Run forever
     time.sleep(0.1)
 
     if GPIO.input(7) == GPIO.HIGH and not button_pressed:
